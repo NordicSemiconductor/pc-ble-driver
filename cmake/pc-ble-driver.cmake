4d6101ebb6b09a7646e6811b77154f22a1a45086
cacu <carles.cufi@nordicsemi.no>
2016-06-04 17:24:18 +0200

    Move boost deps to common cmake file and add an object build

--- cmake/pc-ble-driver.cmake
+++ cmake/pc-ble-driver.cmake
@@ -8,3 +8,10 @@ else()
 endif()
 
 set(PC_BLE_DRIVER_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../include ${CMAKE_CURRENT_LIST_DIR}/../include/internal/transport)
+
+set(Boost_USE_STATIC_LIBS   ON)
+set(Boost_USE_MULTITHREADED ON)
+
+# Find the necessary boost components on system
+find_package ( Boost 1.54.0 REQUIRED COMPONENTS thread system regex date_time chrono )
+
