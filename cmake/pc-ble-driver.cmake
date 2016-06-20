a1f1511006d1a30a4383e3bed07b073845c6cfc6
Carles Cufi <carles.cufi@nordicsemi.no>
2016-06-20 11:05:31 +0200

    Add build information functionality

--- cmake/pc-ble-driver.cmake
+++ cmake/pc-ble-driver.cmake
@@ -67,3 +67,61 @@ set(PC_BLE_DRIVER_HEX_DIR ${PC_BLE_DRIVER_ROOT_DIR}/hex)
 
 # pc-ble-driver include paths
 set(PC_BLE_DRIVER_INCLUDE_DIR ${PC_BLE_DRIVER_ROOT_DIR}/include ${PC_BLE_DRIVER_ROOT_DIR}/../include/internal/transport)
+
+find_package(Git REQUIRED)
+
+function(git_repo_metadata dir commit branch remotes)
+
+    # Get the latest abbreviated commit hash of the working branch
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} log -1 --format=%H
+      WORKING_DIRECTORY ${dir}
+      OUTPUT_VARIABLE GIT_COMMIT_HASH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )    
+
+    set(${commit} ${GIT_COMMIT_HASH} PARENT_SCOPE)
+
+    # Get the current working branch
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+      WORKING_DIRECTORY ${dir}
+      OUTPUT_VARIABLE GIT_BRANCH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    set(${branch} ${GIT_BRANCH} PARENT_SCOPE)
+
+    # Get the Git Remote
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} remote -v
+      WORKING_DIRECTORY ${dir}
+      OUTPUT_VARIABLE GIT_REMOTES
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    set(${remotes} ${GIT_REMOTES} PARENT_SCOPE)
+
+endfunction()
+
+function(build_metadata dir dst)
+
+    cmake_host_system_information(RESULT BUILD_MD_HOSTNAME QUERY HOSTNAME)
+    string(TIMESTAMP BUILD_TIMESTAMP "%Y-%m-%d %H:%M:%S (YY-MM-DD HH:MM:SS, UTC)" UTC)
+
+    set(str "\n")
+    string(CONCAT str ${str} "* System: " ${CMAKE_SYSTEM} "\n") 
+    string(CONCAT str ${str} "* Hostname: " ${BUILD_MD_HOSTNAME} "\n") 
+    string(CONCAT str ${str} "* Timestamp: " ${BUILD_TIMESTAMP} "\n") 
+    string(CONCAT str ${str} "* Generator: " ${CMAKE_GENERATOR} "\n") 
+    string(CONCAT str ${str} "* Build type: " ${CMAKE_BUILD_TYPE} "\n") 
+    string(CONCAT str ${str} "* C Compiler: " ${CMAKE_C_COMPILER_ID} "\n") 
+    string(CONCAT str ${str} "* C++ Compiler: " ${CMAKE_CXX_COMPILER_ID} "\n") 
+    string(CONCAT str ${str} "* CMake version: " ${CMAKE_VERSION} "\n") 
+    string(CONCAT str ${str} "* Boost version: " ${Boost_MAJOR_VERSION} "." ${Boost_MINOR_VERSION} "." ${Boost_SUBMINOR_VERSION} "\n") 
+    string(CONCAT str ${str} "* Boost libs: " ${Boost_LIBRARY_DIRS} "\n") 
+     
+    set(${dst} ${str} PARENT_SCOPE)
+ 
+
+endfunction()
