ecbc7d2c8ce819f39b419dcf46e0fe2cac5acda9
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2016-10-13 15:49:06 +0200

    Add 1Mbps version of sd api 3.0 hex

--- hex/sd_api_v2/bootstrap_sd_api_v2.sh
+++ hex/sd_api_v2/bootstrap_sd_api_v2.sh
@@ -2,7 +2,6 @@
 
 source ../bootstrap.sh
 
-#SDK_LINK='https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v11.x.x/nRF5_SDK_11.0.0_89a8197.zip'
 set_sdk_link 'https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v11.x.x/nRF5_SDK_11.0.0_89a8197.zip'
 
 set_dl_location ../tmp
