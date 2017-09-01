31aab3df73d60793d847a5d8490862416308ce99
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2017-09-01 12:42:09 +0200

    Add script and patch file for v5

---
+++ hex/sd_api_v5/bootstrap_sd_api_v5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Download an patch the nRF5 SDK to compile the connectivity application.
+# Use the nRF5 SDK 12.1 and the SoftDevice API version 3.
+
+ABS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source $ABS_PATH/../bootstrap.sh \
+  -l 'https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v14.x.x/nRF5_SDK_14.0.0_3bcc1f7.zip' \
+  -d "../sdk" \
+  -p 'sd_api_v5/sdk140_connectivity.patch'
