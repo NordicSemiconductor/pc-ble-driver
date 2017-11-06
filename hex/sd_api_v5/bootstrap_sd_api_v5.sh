d2b42319197e895f114f0e9d10f5ab170665cf21
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2017-11-06 11:21:00 +0100

    Add downloading of corresponding DeviceFamilyPack in bootstrap script

--- hex/sd_api_v5/bootstrap_sd_api_v5.sh
+++ hex/sd_api_v5/bootstrap_sd_api_v5.sh
@@ -7,5 +7,6 @@ ABS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source $ABS_PATH/../bootstrap.sh \
   -l 'https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v14.x.x/nRF5_SDK_14.0.0_3bcc1f7.zip' \
+  -f 'https://developer.nordicsemi.com/nRF5_SDK/pieces/nRF_DeviceFamilyPack/NordicSemiconductor.nRF_DeviceFamilyPack.8.14.1.pack' \
   -d "../sdk" \
   -p 'sd_api_v5/sdk140_connectivity.patch'
