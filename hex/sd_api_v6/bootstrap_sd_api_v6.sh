0fa3cd89160054552b696c7617e8baed07030652
Chun Fan <chunfantasy@gmail.com>
2018-04-03 09:47:04 +0200

    Add bootstrap_sd_api_v6.sh

---
+++ hex/sd_api_v6/bootstrap_sd_api_v6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Download an patch the nRF5 SDK to compile the connectivity application.
+# Use the nRF5 SDK 15.0 and the SoftDevice API version 6.
+
+ABS_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source $ABS_PATH/../bootstrap.sh \
+  -l 'https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v15.x.x/nRF5_SDK_15.0.0_a53641a.zip' \
+  -f 'https://developer.nordicsemi.com/nRF5_SDK/pieces/nRF_DeviceFamilyPack/NordicSemiconductor.nRF_DeviceFamilyPack.8.16.0.pack' \
+  -d "../sdk" \
+  -p 'sd_api_v6/sdk140_connectivity.patch'
