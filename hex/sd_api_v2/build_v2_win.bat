7da69616d32f7fa489c63b60277a19f35a74e0f1
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2018-04-30 14:56:13 +0200

    Modify env variable

--- hex/sd_api_v2/build_v2_win.bat
+++ hex/sd_api_v2/build_v2_win.bat
@@ -3,6 +3,8 @@
 @SET scriptpath=%~dp0
 @SET rootpath=%scriptpath%..\..
 
+IF "%CONN_VERSION%"=="" (SET CONN_VERSION=0.0.0)
+
 REM Run bootstrap script
 cd %scriptpath%
 "c:\program files\git\bin\bash.exe" bootstrap_sd_api_v2.sh
@@ -21,7 +23,7 @@ cd %rootpath%\sdk\s11\examples\ble_central_and_peripheral\ble_connectivity\pca10
 \Keil_v5\UV4\UV4.exe -b ble_connectivity_s130_hci_pca10028.uvprojx -j0 -o log_1m.txt
 
 REM Merge hex
-"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf51422_xxac_s130.hex %rootpath%\sdk\s121\components\softdevice\s130\hex\s130_nrf51_2.0.1_softdevice.hex -o %rootpath%\sdk\connectivity_${bamboo.CONN_VERSION}_1m_with_s130_2.0.1.hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf51422_xxac_s130.hex %rootpath%\sdk\s121\components\softdevice\s130\hex\s130_nrf51_2.0.1_softdevice.hex -o %rootpath%\sdk\connectivity_%CONN_VERSION%_1m_with_s130_2.0.1.hex
 
 REM String replace baudrate
 "c:\program files\git\bin\bash.exe" -c "sed -i -e 's/UART_BAUDRATE_BAUDRATE_Baud1M$/UART_BAUDRATE_BAUDRATE_Baud115200/' ../../../../../../../s11/components/serialization/common/ser_config.h"
@@ -30,6 +32,6 @@ REM Compile
 \Keil_v5\UV4\UV4.exe -b ble_connectivity_s130_hci_pca10028.uvprojx -j0 -o log_115k2.txt
 
 REM Merge hex
-"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf51422_xxac_s130.hex %rootpath%\sdk\s121\components\softdevice\s130\hex\s130_nrf51_2.0.1_softdevice.hex -o %rootpath%\sdk\connectivity_${bamboo.CONN_VERSION}_115k2_with_s130_2.0.1.hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf51422_xxac_s130.hex %rootpath%\sdk\s121\components\softdevice\s130\hex\s130_nrf51_2.0.1_softdevice.hex -o %rootpath%\sdk\connectivity_%CONN_VERSION%_115k2_with_s130_2.0.1.hex
 
 @cd %startpath%
