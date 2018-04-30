4a7794e5dfec91bf95b916fc6f355c03ef1f952d
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2018-04-30 14:57:50 +0200

    Modify env variable for v3

--- hex/sd_api_v3/build_v3_win.bat
+++ hex/sd_api_v3/build_v3_win.bat
@@ -3,6 +3,8 @@
 @SET scriptpath=%~dp0
 @SET rootpath=%scriptpath%..\..
 
+IF "%CONN_VERSION%"=="" (SET CONN_VERSION=0.0.0)
+
 REM Run bootstrap script
 cd %scriptpath%
 
@@ -18,7 +20,7 @@ REM Compile
 \Keil_v5\UV4\UV4.exe -b ble_connectivity_s132_hci_pca10040.uvprojx -j0 -o log_1m.txt
 
 REM Merge hex
-"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf52832_xxaa.hex %rootpath%\sdk\s122\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex -o %rootpath%\sdk\connectivity_${bamboo.CONN_VERSION}_1m_with_s132_3.0.hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf52832_xxaa.hex %rootpath%\sdk\s122\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex -o %rootpath%\sdk\connectivity_%CONN_VERSION%_1m_with_s132_3.0.hex
 
 REM String replace baudrate
 "c:\program files\git\bin\bash.exe" -c "sed -i -e 's/UART_BAUDRATE_BAUDRATE_Baud1M$/UART_BAUDRATE_BAUDRATE_Baud115200/' ../../../../../../../s122/components/serialization/common/ser_config.h"
@@ -27,6 +29,6 @@ REM Compile
 \Keil_v5\UV4\UV4.exe -b ble_connectivity_s132_hci_pca10040.uvprojx -j0 -o log_115k2.txt
 
 REM Merge hex
-"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf52832_xxaa.hex %rootpath%\sdk\s122\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex -o %rootpath%\sdk\s122\connectivity_${bamboo.CONN_VERSION}_115k2_with_s132_3.0.hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf52832_xxaa.hex %rootpath%\sdk\s122\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex -o %rootpath%\sdk\s122\connectivity_%CONN_VERSION%_115k2_with_s132_3.0.hex
 
 @cd %startpath%
