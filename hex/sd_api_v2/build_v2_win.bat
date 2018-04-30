a1dc6dc2809f8cfcd6fa415a9dc82b47f0166d4d
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2018-04-30 13:54:53 +0200

    Add build script for v2 for win

---
+++ hex/sd_api_v2/build_v2_win.bat
@@ -0,0 +1,35 @@
+@SETLOCAL
+@SET startpath=%cd%
+@SET scriptpath=%~dp0
+@SET rootpath=%scriptpath%..\..
+
+REM Run bootstrap script
+cd %scriptpath%
+"c:\program files\git\bin\bash.exe" bootstrap_sd_api_v2.sh
+
+REM Download SDK 12 to get newer softdevice version
+"c:\program files\git\bin\bash.exe" sdk_download.sh
+
+@REM Workaround to reduce path length before build
+cd %rootpath%\sdk
+rename nRF5_SDK_11.0.0_89a8197 s11
+rename nRF5_SDK_12.1.0_0d23e2a s121
+
+REM Compile
+cd %rootpath%\sdk\s11\examples\ble_central_and_peripheral\ble_connectivity\pca10028\ser_s130_hci\arm5_no_packs
+
+\Keil_v5\UV4\UV4.exe -b ble_connectivity_s130_hci_pca10028.uvprojx -j0 -o log_1m.txt
+
+REM Merge hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf51422_xxac_s130.hex %rootpath%\sdk\s121\components\softdevice\s130\hex\s130_nrf51_2.0.1_softdevice.hex -o %rootpath%\sdk\connectivity_${bamboo.CONN_VERSION}_1m_with_s130_2.0.1.hex
+
+REM String replace baudrate
+"c:\program files\git\bin\bash.exe" -c "sed -i -e 's/UART_BAUDRATE_BAUDRATE_Baud1M$/UART_BAUDRATE_BAUDRATE_Baud115200/' ../../../../../../../s11/components/serialization/common/ser_config.h"
+
+REM Compile
+\Keil_v5\UV4\UV4.exe -b ble_connectivity_s130_hci_pca10028.uvprojx -j0 -o log_115k2.txt
+
+REM Merge hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf51422_xxac_s130.hex %rootpath%\sdk\s121\components\softdevice\s130\hex\s130_nrf51_2.0.1_softdevice.hex -o %rootpath%\sdk\connectivity_${bamboo.CONN_VERSION}_115k2_with_s130_2.0.1.hex
+
+@cd %startpath%
