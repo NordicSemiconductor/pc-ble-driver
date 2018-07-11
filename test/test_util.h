15a5e7537a371b7fd73b4cdf79052d79b9a16cc5
Ken A. Redergård <kenandr@gmail.com>
2018-07-11 13:51:42 +0200

    Move reusable test functionality into separate file (#119)
    The baud rate and ble_address_to_string_convert is reusable across different tests. Moving functionality to use default platform baudrate and converting BLE address to a header file where it can be reused.

--- test/test_util.h
+++ test/test_util.h
@@ -3,7 +3,7 @@
 
 namespace testutil
 {
-    std::string convertToString(const std::vector<uint8_t> &data) {
+    static std::string convertToString(const std::vector<uint8_t> &data) {
         std::stringstream ss;
         ss << std::hex << std::setfill('0');
 
@@ -13,6 +13,19 @@ namespace testutil
         }
 
         return ss.str();
+    }
+
+    static std::string ble_address_to_string_convert(ble_gap_addr_t address)
+    {
+        const int address_length = 6;
+        std::stringstream retval;
+
+        for (int i = sizeof(address.addr) - 1; i >= 0; --i)
+        {
+            retval << std::hex << static_cast<unsigned int>(address.addr[i]);
+        }
+        
+        return retval.str();
     }
 }
 
