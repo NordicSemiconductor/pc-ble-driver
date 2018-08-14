b3988d8bdd8ed903415661e763003639a4eb2acd
Bjørn Inge Hanssen <bjorn.inge.hanssen@nordicsemi.no>
2018-08-14 10:22:06 +0200

    Update reference to softdevice version
    Add new softdevice 3.1 hex and license
    
    Remove obsoleted softdevice 3.0 hex and license

--- hex/sd_api_v3/build_v3_usb_win.bat
+++ hex/sd_api_v3/build_v3_usb_win.bat
@@ -27,12 +27,12 @@ cd %rootpath%\sdk\s15\examples\connectivity\ble_connectivity\pca10059\ser_s132v3
 \Keil_v5\UV4\UV4.exe -b ble_connectivity_s132v3_usb_hci_pca10059.uvprojx -j0 -o log_usb.txt
 
 @ECHO Copy HEX only
-copy _build\nrf52840_xxaa.hex %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_for_s132_3.0.hex
+copy _build\nrf52840_xxaa.hex %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_for_s132_3.hex
 
 @ECHO Merge hex
-"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf52840_xxaa.hex %rootpath%\sdk\s122\components\softdevice\s132\hex\s132_nrf52_3.0.0_softdevice.hex -o %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_with_s132_3.0.hex
+"c:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin\mergehex.exe" -m _build\nrf52840_xxaa.hex %rootpath%\sdk\s122\components\softdevice\s132\hex\s132_nrf52_3.1.0_softdevice.hex -o %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_with_s132_3.1.hex
 
 @ECHO Checking that the output files exist
-@IF NOT EXIST %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_for_s132_3.0.hex EXIT /b 1
-@IF NOT EXIST %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_with_s132_3.0.hex EXIT /b 1
+@IF NOT EXIST %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_for_s132_3.hex EXIT /b 1
+@IF NOT EXIST %rootpath%\sdk\connectivity_%CONN_VERSION%_usb_with_s132_3.1.hex EXIT /b 1
 @ECHO Success
