267fd1aaf2a66f5ebf86681929b592e521fae1bf
Ken A. Redergård <ken.redergard@nordicsemi.no>
2018-09-12 13:34:47 +0200

    Fix patches, change directory structure
    This commit fixes patches that are applied to SDKv11 and SDKv15.
    The contained empty lines without spaces that prevented the git command to apply parts of the patches.
    
    The patches are also altered to support changing of connectivity firmware version number.
    
    The changes to the directory structure make the files more intuitive to understand.

---
+++ hex/cmake/build_cmd_ser_config.cmake
@@ -0,0 +1,40 @@
+#[[
+    This script is invoked from CMakeLists in this directory during the build steps 
+    for the connectivity firmware.
+]]
+
+if(NOT DEFINED SDK_VERSION)
+    message(FATAL_ERROR "SDK_VERSION not provided.")
+endif()
+
+if(NOT DEFINED SER_CONFIG_IN_PATH)
+    message(FATAL_ERROR "SER_CONFIG_IN_PATH not provided.")
+endif()
+
+if(NOT DEFINED BAUD_RATE)
+    message(FATAL_ERROR "BAUD_RATE not provided.")
+endif()
+
+if(SDK_VERSION STREQUAL "11")
+    if(BAUD_RATE STREQUAL "115k2")
+        set(SER_PHY_UART_BAUDRATE "UART_BAUDRATE_BAUDRATE_Baud115200")
+    elseif(BAUD_RATE STREQUAL "1m" OR BAUD_RATE STREQUAL "usb")
+        set(SER_PHY_UART_BAUDRATE "UART_BAUDRATE_BAUDRATE_Baud1M")
+    endif()
+elseif(SDK_VERSION STREQUAL "15")
+    if(BAUD_RATE STREQUAL "115k2")
+        set(SER_PHY_UART_BAUDRATE_VAL "115200")
+    elseif(BAUD_RATE STREQUAL "1m" OR BAUD_RATE STREQUAL "usb")
+        set(SER_PHY_UART_BAUDRATE_VAL "1000000")
+    endif()
+endif()
+
+if(NOT DEFINED SER_PHY_UART_BAUDRATE AND NOT DEFINED SER_PHY_UART_BAUDRATE_VAL)
+    message(FATAL_ERROR "Not able to find baud rate for SDK ${SDK_VERSION}.")
+endif()
+
+get_filename_component(SER_CONFIG_DIR "${SER_CONFIG_IN_PATH}" DIRECTORY)
+set(SER_CONFIG_PATH "${SER_CONFIG_DIR}/ser_config.h")
+
+#message(STATUS "Configuring ${SER_CONFIG_IN_PATH} with baud rate ${SER_PHY_UART_BAUDRATE}${SER_PHY_UART_BAUDRATE_VAL}, output file ${SER_CONFIG_PATH}")
+configure_file(${SER_CONFIG_IN_PATH} ${SER_CONFIG_PATH} @ONLY)
