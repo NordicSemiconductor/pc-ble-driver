6710e5a2ff180f665ab3eca3fac8baf5222982d0
Ken A. Redergård <ken.redergard@nordicsemi.no>
2018-10-03 16:33:28 +0200

    Update connectivity firmware compilation
    This commit contains:
    - relevant changes from branch feature/use-argmcc-for-connfw to fix a SDK patching issue
    - support for PCA blacklisting
    - connectivity firmware compilation from root CMakeLists.txt file
    - connectivity firmware version sharing between CMake project files

--- cmake/nrf-apply-patches.cmake
+++ cmake/nrf-apply-patches.cmake
@@ -3,16 +3,34 @@ function(nrf_apply_patches)
 
     find_program(GIT NAMES git git.cmd)
     set(PATCHNUM 0)
+
+    # Check if we are inside a git work tree, if so, git apply will
+    # not apply the patch if the files to patcha are not in the git
+    # index. We only want to use git as a patch utlity without any
+    # of the git features.
+    execute_process(
+        COMMAND "${GIT}" rev-parse --is-inside-work-tree
+        WORKING_DIRECTORY ${nrf_apply_patches_SOURCE_PATH}
+        RESULT_VARIABLE error_code
+    )
+     if(error_code)
+        message(STATUS "Not inside a git work tree, applying patches.")
+        message(STATUS "Applying patches in directory ${nrf_apply_patches_SOURCE_PATH}")
+    else()
+        message(FATAL_ERROR "Inside git work tree, not able to apply patches. Path is ${nrf_apply_patches_SOURCE_PATH}.")
+    endif()
+
     foreach(PATCH ${nrf_apply_patches_PATCHES})
         message(STATUS "Applying patch ${PATCH}")
+
         execute_process(
-            COMMAND "${GIT}" apply "${PATCH}" --ignore-whitespace --whitespace=nowarn --unsafe-paths
+            COMMAND "${GIT}" apply "${PATCH}" --ignore-whitespace
             WORKING_DIRECTORY ${nrf_apply_patches_SOURCE_PATH}
             RESULT_VARIABLE error_code
         )
 
         if(error_code)
-            message(STATUS "Applying patch failed with error \"${error_code}\". This is expected if this patch was previously applied.")
+            message(WARNING "Applying patch failed with error \"${error_code}\". This is expected if this patch was previously applied.")
         endif()
 
         math(EXPR PATCHNUM "${PATCHNUM}+1")
