12ccdbbc1045579c8a4dd748c108e31b6001afbe
Ken A. Redergård <ken.redergard@nordicsemi.no>
2018-11-02 07:41:33 +0100

    Add CLI to tests

--- test/softdevice_api/test_main.cpp
+++ test/softdevice_api/test_main.cpp
@@ -2,12 +2,34 @@
 
 #include <iostream>
 #include <mutex>
-#include <sstream>
 
 std::ostream &nrfLogStream(std::cout);
 std::mutex nrfLogMutex;
 
-#include <internal/log.h>
-
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch2/catch.hpp"
+
+int main(int argc, char *argv[])
+{
+    Catch::Session session;
+
+    std::string serialPortA;
+    std::string serialPortB;
+    uint32_t iterations;
+
+    using namespace Catch::clara;
+
+    const auto cli = session.cli() |
+                     Opt(serialPortA, "serial port A")["-pa"]["--porta"]("Serial port A") |
+                     Opt(serialPortB, "serial port B")["-pb"]["--portb"]("Serial port B") |
+                     Opt(iterations, "iterations")["-it"]["--iterations"](
+                         "Number of iterations. "
+                         "Ignored for tests not having iterations.");
+
+    session.cli(cli);
+
+    const auto exitCode = session.applyCommandLine(argc, argv);
+
+    if (exitCode != 0)
+        return exitCode;
+}
