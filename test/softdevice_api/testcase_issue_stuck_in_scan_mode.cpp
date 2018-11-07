8e55bc4f0a1ea8679d510971d35546d8d098afb7
Ken A. Redergård <ken.redergard@nordicsemi.no>
2018-11-07 08:49:38 +0100

    Remove unnecessary section

--- test/softdevice_api/testcase_issue_stuck_in_scan_mode.cpp
+++ test/softdevice_api/testcase_issue_stuck_in_scan_mode.cpp
@@ -66,95 +66,90 @@ TEST_CASE(CREATE_TEST_NAME_AND_TAGS(issue_stuck_in_scan_mode,
     const auto serialPort = env.serialPorts.at(0);
     INFO("Serial port used: " << serialPort.port);
 
-    SECTION("reproduce")
+    const auto baudRate = serialPort.baudRate;
+
+    const auto scanIterations = 10;
+    INFO("Purpose of this test:");
+    INFO("1) Assert that closing the adapter when scan is running does not prevent opening the "
+         "adapter again.");
+    INFO("2) Assert that advertisement reports are received all the time (timeout is set to "
+         "0x00)");
+    INFO("Running " << scanIterations << " adapter open -> scan -> adapter close iterations");
+
+    for (auto i = 0; i < scanIterations; i++)
     {
-        const auto baudRate = serialPort.baudRate;
+        auto adv_report_count = 0;
 
-        const auto scanIterations = 10;
-        INFO("Purpose of this test:");
-        INFO("1) Assert that closing the adapter when scan is running does not prevent opening the "
-             "adapter again.");
-        INFO("2) Assert that advertisement reports are received all the time (timeout is set to "
-             "0x00)");
-        INFO("Running " << scanIterations << " adapter open -> scan -> adapter close iterations");
+        auto c = std::make_shared<testutil::AdapterWrapper>(testutil::Central, serialPort.port,
+                                                            baudRate);
 
-        for (auto i = 0; i < scanIterations; i++)
-        {
-            auto adv_report_count = 0;
+        NRF_LOG(c->role() << " Starting scan iteration #" << std::dec << static_cast<uint32_t>(i)
+                          << " of " << static_cast<uint32_t>(scanIterations));
 
-            auto c = std::make_shared<testutil::AdapterWrapper>(testutil::Central, serialPort.port,
-                                                                baudRate);
+        REQUIRE(sd_rpc_log_handler_severity_filter_set(c->unwrap(), env.driverLogLevel) ==
+                NRF_SUCCESS);
 
-            NRF_LOG(c->role() << " Starting scan iteration #" << std::dec
-                              << static_cast<uint32_t>(i) << " of "
-                              << static_cast<uint32_t>(scanIterations));
+        // Scan forever
+        c->scratchpad.scan_param.timeout = 0;
 
-            REQUIRE(sd_rpc_log_handler_severity_filter_set(c->unwrap(), env.driverLogLevel) ==
-                    NRF_SUCCESS);
-
-            // Scan forever
-            c->scratchpad.scan_param.timeout = 0;
-
-            c->setGapEventCallback([&](const uint16_t eventId,
-                                       const ble_gap_evt_t *gapEvent) -> bool {
-                switch (eventId)
-                {
-                    case BLE_GAP_EVT_ADV_REPORT:
-                        adv_report_count++;
-                        NRF_LOG("adv_report_count: " << adv_report_count);
+        c->setGapEventCallback([&](const uint16_t eventId, const ble_gap_evt_t *gapEvent) -> bool {
+            switch (eventId)
+            {
+                case BLE_GAP_EVT_ADV_REPORT:
+                    adv_report_count++;
+                    NRF_LOG("adv_report_count: " << adv_report_count);
 #if NRF_SD_BLE_API == 6
+                    {
+                        const auto err_code = c->startScan(true);
+                        if (err_code != NRF_SUCCESS)
                         {
-                            const auto err_code = c->startScan(true);
-                            if (err_code != NRF_SUCCESS)
-                            {
-                                NRF_LOG(c->role()
-                                        << " startScan in BLE_GAP_EVT_ADV_REPORT failed, err_code "
-                                        << err_code);
-                                error = true;
-                            }
+                            NRF_LOG(c->role()
+                                    << " startScan in BLE_GAP_EVT_ADV_REPORT failed, err_code "
+                                    << err_code);
+                            error = true;
                         }
+                    }
 #endif
-                        adv_report_received = std::chrono::steady_clock::now();
+                    adv_report_received = std::chrono::steady_clock::now();
 
-                        return true;
-                    case BLE_GAP_EVT_TIMEOUT:
-                        if (gapEvent->params.timeout.src == BLE_GAP_TIMEOUT_SRC_SCAN)
+                    return true;
+                case BLE_GAP_EVT_TIMEOUT:
+                    if (gapEvent->params.timeout.src == BLE_GAP_TIMEOUT_SRC_SCAN)
+                    {
+                        const auto err_code = c->startScan();
+
+                        if (err_code != NRF_SUCCESS)
                         {
-                            const auto err_code = c->startScan();
-
-                            if (err_code != NRF_SUCCESS)
-                            {
-                                NRF_LOG(c->role() << " Scan start error, err_code " << err_code);
-                                error = true;
-                            }
+                            NRF_LOG(c->role() << " Scan start error, err_code " << err_code);
+                            error = true;
                         }
-                        return true;
-                    default:
-                        return false;
-                }
-            });
+                    }
+                    return true;
+                default:
+                    return false;
+            }
+        });
 
-            REQUIRE(c->open() == NRF_SUCCESS);
-            REQUIRE(c->configure() == NRF_SUCCESS);
-            REQUIRE(c->startScan() == NRF_SUCCESS);
+        REQUIRE(c->open() == NRF_SUCCESS);
+        REQUIRE(c->configure() == NRF_SUCCESS);
+        REQUIRE(c->startScan() == NRF_SUCCESS);
 
-            std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::this_thread::sleep_for(std::chrono::seconds(5));
 
-            // Check that we have recently received an advertisement report
-            auto now = std::chrono::steady_clock::now();
-            auto silence_duration =
-                std::chrono::duration_cast<std::chrono::milliseconds>(now - adv_report_received)
-                    .count();
-            REQUIRE(silence_duration < 1000);
+        // Check that we have recently received an advertisement report
+        auto now = std::chrono::steady_clock::now();
+        auto silence_duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(now - adv_report_received)
+                .count();
+        REQUIRE(silence_duration < 1000);
 
-            REQUIRE(error == false);
+        REQUIRE(error == false);
 
-            // Cleanup current adapter connection
-            REQUIRE(c->close() == NRF_SUCCESS);
-            sd_rpc_adapter_delete(c->unwrap());
+        // Cleanup current adapter connection
+        REQUIRE(c->close() == NRF_SUCCESS);
+        sd_rpc_adapter_delete(c->unwrap());
 
-            NRF_LOG(c->role() << " Scan iteration #" << std::dec << static_cast<uint32_t>(i)
-                              << " of " << static_cast<uint32_t>(scanIterations) << " completed");
-        }
+        NRF_LOG(c->role() << " Scan iteration #" << std::dec << static_cast<uint32_t>(i) << " of "
+                          << static_cast<uint32_t>(scanIterations) << " completed");
     }
 }
