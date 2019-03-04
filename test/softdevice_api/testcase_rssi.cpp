e4c2047e453251dd6786bf40c76d77a92d7737f3
Ken A. Redergård <64542+kenr@users.noreply.github.com>
2019-03-04 10:00:37 +0100

    Check if status callback returns errors (#207)

--- test/softdevice_api/testcase_rssi.cpp
+++ test/softdevice_api/testcase_rssi.cpp
@@ -340,6 +340,22 @@ TEST_CASE(CREATE_TEST_NAME_AND_TAGS(rssi, [PCA10028][PCA10031][PCA10040][PCA1005
         }
     });
 
+    c->setStatusCallback([&](const sd_rpc_app_status_t code, const std::string &message) {
+        if (code == PKT_DECODE_ERROR || code == PKT_SEND_MAX_RETRIES_REACHED ||
+            code == PKT_UNEXPECTED)
+        {
+            centralError = true;
+        }
+    });
+
+    p->setStatusCallback([&](const sd_rpc_app_status_t code, const std::string &message) {
+        if (code == PKT_DECODE_ERROR || code == PKT_SEND_MAX_RETRIES_REACHED ||
+            code == PKT_UNEXPECTED)
+        {
+            peripheralError = true;
+        }
+    });
+
     // Open the adapters
     REQUIRE(c->open() == NRF_SUCCESS);
     REQUIRE(p->open() == NRF_SUCCESS);
