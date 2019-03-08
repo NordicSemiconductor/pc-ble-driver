f9263e70b414ef177db86cf778f20ad280a5f031
Ken A. Redergård <64542+kenr@users.noreply.github.com>
2019-03-08 18:02:17 +0100

    Make tidy target use .clang-tidy config, enable clang-tidy on win32

--- cmake/clang-dev-tools.cmake
+++ cmake/clang-dev-tools.cmake
@@ -1,54 +1,50 @@
 if(WIN32)
-	# CMAKE_EXPORT_COMPILE_COMMANDS is not working on Windows
-	message(STATUS "clang-tidy is not supported on Windows")
+    if (NOT CMAKE_GENERATOR STREQUAL "Ninja")
+        set(NOT_SUPPORTED_MESSAGE "clang-tidy is only supported with Ninja generator on Microsoft Windows.")
+        message(STATUS "${NOT_SUPPORTED_MESSAGE}")
 
-	# For the target not to fail:
-	add_custom_target(tidy COMMAND echo "not supported on Windows")
+        # For the target not to fail:
+        add_custom_target(tidy COMMAND echo "${NOT_SUPPORTED_MESSAGE}")
+        return()
+    else()
+        find_package (Python COMPONENTS Interpreter)
 
-else()
-	find_program(CLANG_TIDY NAMES run-clang-tidy.py run-clang-tidy-6.0.py)
-	if(NOT CLANG_TIDY)
-
-		message(STATUS "Did not find clang-tidy, target tidy is disabled.")
-		message(STATUS "If clang-tidy is installed, make sure run-clang-tidy.py is in PATH")
-
-		# For the target not to fail:
-		add_custom_target(tidy COMMAND echo "Clang-tidy is not installed")
-
-	else()
-		message(STATUS "Found clang-tidy, use \"make tidy\" to run it.")
-
-		# This will create build/compile_commands.json
-		set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-		set(CLANG_TIDY_CHECKS "*")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-llvm-header-guard")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-llvm-include-order")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-llvm-namespace-comment")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-readability-else-after-return")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-misc-macro-parentheses")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-clang-analyzer-alpha.core.CastToStruct")
-		# Modernize
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-modernize-raw-string-literal")
-		# CPP Core Guidelines
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-cppcoreguidelines-pro-bounds-array-to-pointer-decay")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-cppcoreguidelines-pro-bounds-constant-array-index")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-cppcoreguidelines-pro-bounds-pointer-arithmetic")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-cppcoreguidelines-pro-type-member-init") # as of https://llvm.org/bugs/show_bug.cgi?id=31039
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-cppcoreguidelines-pro-type-reinterpret-cast")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-cppcoreguidelines-pro-type-vararg")
-		# Google
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-google-readability-namespace-comments")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-google-readability-braces-around-statements,-readability-braces-around-statements")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-google-readability-todo")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-google-runtime-int")
-		set(CLANG_TIDY_CHECKS "${CLANG_TIDY_CHECKS},-google-runtime-references")
-		set(CLANG_TIDY_CHECKS "-checks='${CLANG_TIDY_CHECKS}'")
-
-		add_custom_target(tidy
-			COMMAND ${CLANG_TIDY} ${CLANG_TIDY_CHECKS}
-			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build/
-		)
-	endif()
+        if(NOT Python_Interpreter_FOUND)
+            set(PYTHON_MISSING_MESSAGE "Python interpreter not found, it is required for run-clang-tidy.py")
+            message(STATUS "${PYTHON_MISSING_MESSAGE}")
+            add_custom_target(tidy COMMAND echo "${PYTHON_MISSING_MESSAGE}")
+            return()
+        endif()
+    endif()
 endif()
 
+find_program(CLANG_TIDY NAMES run-clang-tidy.py run-clang-tidy-6.0.py)
+
+if(NOT CLANG_TIDY)
+    message(STATUS "Did not find clang-tidy, target tidy is disabled.")
+    message(STATUS "If clang-tidy is installed, make sure run-clang-tidy.py and clang-tidy is in PATH")
+
+    # For the target not to fail:
+    add_custom_target(tidy COMMAND echo "Clang-tidy is not installed")
+else()
+    message(STATUS "Found clang-tidy, use \"cmake --build . --target tidy\" to run it.")
+
+    # This will create build/compile_commands.json
+    set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+    # Configuration of rules are picked up from .clang-tidy
+    message(STATUS "Picking up clang-tidy rules from ${CMAKE_SOURCE_DIR}/.clang-tidy")
+    set(CLANG_TIDY_ARGS "-header-filter=.*")
+
+    if (WIN32)
+        add_custom_target(tidy
+            COMMAND ${Python_EXECUTABLE} ${CLANG_TIDY} ${CLANG_TIDY_ARGS} .
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        )
+    else()
+        add_custom_target(tidy
+            COMMAND ${CLANG_TIDY} ${CLANG_TIDY_ARGS} .
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        )
+    endif()
+endif()
