f14b30b76ccea4993306413ba9b43ab77b6f81da
Ken A. Redergård <64542+kenr@users.noreply.github.com>
2021-03-26 17:28:21 +0100

    Add better logging

--- src/common/transport/serialization_transport.cpp
+++ src/common/transport/serialization_transport.cpp
@@ -1,40 +1,3 @@
-/*
- * Copyright (c) 2016 Nordic Semiconductor ASA
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *   1. Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- *   2. Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- *   3. Neither the name of Nordic Semiconductor ASA nor the names of other
- *   contributors to this software may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
- *
- *   4. This software must only be used in or with a processor manufactured by Nordic
- *   Semiconductor ASA, or in or with a processor manufactured by a third party that
- *   is used in combination with a processor manufactured by Nordic Semiconductor.
- *
- *   5. Any software provided in binary or object form under this license must not be
- *   reverse engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "serialization_transport.h"
 
 #include "ble.h"
@@ -227,7 +190,8 @@ void SerializationTransport::drainEventQueue()
     }
     catch (const std::exception &ex)
     {
-        LogHelper::tryToLogException(spdlog::level::err, ex, "Error in SerializationTransport::drainEventQueue");
+        LogHelper::tryToLogException(spdlog::level::err, ex,
+                                     "Error in SerializationTransport::drainEventQueue");
     }
 }
 
@@ -296,7 +260,8 @@ void SerializationTransport::eventHandlingRunner() noexcept
     }
     catch (const std::exception &e)
     {
-        LogHelper::tryToLogException(spdlog::level::critical, e, "Error in SerializationTransport::eventHandlingRunner");
+        LogHelper::tryToLogException(spdlog::level::critical, e,
+                                     "Error in SerializationTransport::eventHandlingRunner");
     }
 }
 
@@ -318,12 +283,14 @@ void SerializationTransport::readHandler(const uint8_t *data, const size_t lengt
             }
             else
             {
-                getLogger()->error("Received SERIALIZATION_RESPONSE with a packet that is larger than the allocated buffer.");
+                getLogger()->error("Received SERIALIZATION_RESPONSE with a packet that is larger "
+                                   "than the allocated buffer.");
             }
         }
         else
         {
-            getLogger()->error("Received SERIALIZATION_RESPONSE but command did not provide a buffer for the reply.");
+            getLogger()->error("Received SERIALIZATION_RESPONSE but command did not provide a "
+                               "buffer for the reply.");
         }
 
         std::lock_guard<std::mutex> responseGuard(responseMutex);
--- src/common/transport/uart_transport.cpp
+++ src/common/transport/uart_transport.cpp
@@ -1,40 +1,3 @@
-/*
- * Copyright (c) 2016-2019 Nordic Semiconductor ASA
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *   1. Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- *   2. Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- *   3. Neither the name of Nordic Semiconductor ASA nor the names of other
- *   contributors to this software may be used to endorse or promote products
- *   derived from this software without specific prior written permission.
- *
- *   4. This software must only be used in or with a processor manufactured by Nordic
- *   Semiconductor ASA, or in or with a processor manufactured by a third party that
- *   is used in combination with a processor manufactured by Nordic Semiconductor.
- *
- *   5. Any software provided in binary or object form under this license must not be
- *   reverse engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "uart_transport.h"
 
 #include "log_helper.h"
@@ -120,12 +83,10 @@ struct UartTransport::impl : Transport
         }
         else
         {
-            std::stringstream message;
-            message << "serial port read failed on port " << uartSettingsBoost.getPortName()
-                    << ". ";
-            message << "Error: " << errorCode.message() << " [" << errorCode.value() << "]";
-
-            status(IO_RESOURCES_UNAVAILABLE, message.str());
+            const auto message = fmt::format("serial port read failed on port {}. Error: {} [{}]",
+                                             uartSettingsBoost.getPortName(), errorCode.message(),
+                                             errorCode.value());
+            status(IO_RESOURCES_UNAVAILABLE, message.c_str());
         }
     }
 
@@ -374,13 +335,9 @@ struct UartTransport::impl : Transport
 
         try
         {
-            /*            getLogger()->info("Successfully opened {}. Baud rate: {}. Flow control:
-               {}. Parity: {}", uartSettingsBoost.getPortName(), uartSettingsBoost.getBaudRate(),
-                                          uartSettingsBoost.getFlowControl(),
-               uartSettingsBoost.getParity());*/
-
-            getLogger()->info("Successfully opened {}. Baud rate: {}.",
-                              uartSettingsBoost.getPortName(), uartSettingsBoost.getBaudRate());
+            getLogger()->info("Successfully opened {}. Baud rate: {}. Flow control: {}. Parity: {}",
+                              uartSettingsBoost.getPortName(), uartSettingsBoost.getBaudRate(),
+                              uartSettingsBoost.getFlowControl(), uartSettingsBoost.getParity());
         }
         catch (...)
         {
