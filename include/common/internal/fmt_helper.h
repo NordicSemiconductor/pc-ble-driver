a4917f6bdeb1dc1557b59401c73803090dfdfb49
Ken A. Redergård <64542+kenr@users.noreply.github.com>
2021-03-26 17:25:52 +0100

    Fix user defined fmt formatter

--- include/common/internal/fmt_helper.h
+++ include/common/internal/fmt_helper.h
@@ -21,39 +21,39 @@ template <> struct formatter<h5_state>
         switch (state)
         {
             case h5_state::STATE_START:
-                format_to(ctx.out(), "STATE_START");
+                return format_to(ctx.out(), "STATE_START");
                 break;
             case h5_state::STATE_RESET:
-                format_to(ctx.out(), "STATE_RESET");
+                return format_to(ctx.out(), "STATE_RESET");
                 break;
             case h5_state::STATE_UNINITIALIZED:
-                format_to(ctx.out(), "STATE_UNINITIALIZED");
+                return format_to(ctx.out(), "STATE_UNINITIALIZED");
                 break;
             case h5_state::STATE_INITIALIZED:
-                format_to(ctx.out(), "STATE_INITIALIZED");
+                return format_to(ctx.out(), "STATE_INITIALIZED");
                 break;
             case h5_state::STATE_ACTIVE:
-                format_to(ctx.out(), "STATE_ACTIVE");
+                return format_to(ctx.out(), "STATE_ACTIVE");
                 break;
             case h5_state::STATE_FAILED:
-                format_to(ctx.out(), "STATE_FAILED");
+                return format_to(ctx.out(), "STATE_FAILED");
                 break;
             case h5_state::STATE_CLOSED:
-                format_to(ctx.out(), "STATE_CLOSED");
+                return format_to(ctx.out(), "STATE_CLOSED");
                 break;
             case h5_state::STATE_NO_RESPONSE:
-                format_to(ctx.out(), "STATE_NO_RESPONSE");
+                return format_to(ctx.out(), "STATE_NO_RESPONSE");
                 break;
             case h5_state::STATE_UNKNOWN:
-                format_to(ctx.out(), "STATE_UNKNOWN");
+                return format_to(ctx.out(), "STATE_UNKNOWN");
                 break;
             default:
-                format_to(ctx.out(), "UNKNOWN[{:#04x}]", static_cast<uint32_t>(state));
+                return format_to(ctx.out(), "UNKNOWN[{:#04x}]", static_cast<uint32_t>(state));
         }
     }
 };
 
-template <> struct formatter<std::vector<uint8_t>>
+template <> struct formatter<h5_payload_t>
 {
     // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
     constexpr auto parse(format_parse_context &ctx)
@@ -61,10 +61,9 @@ template <> struct formatter<std::vector<uint8_t>>
         return ctx.begin();
     }
 
-    template <typename FormatContext>
-    auto format(const std::vector<uint8_t> &payload, FormatContext &ctx)
+    template <typename FormatContext> auto format(const h5_payload_t &payload, FormatContext &ctx)
     {
-        format_to(ctx.out(), "{:#04x}", join(payload, ", "));
+        return format_to(ctx.out(), "{:#04x}", join(payload, ", "));
     }
 };
 
@@ -81,31 +80,31 @@ template <> struct formatter<h5_pkt_type>
         switch (pkt_type)
         {
             case h5_pkt_type::ACK_PACKET:
-                format_to(ctx.out(), "ACK");
+                return format_to(ctx.out(), "ACK");
                 break;
             case h5_pkt_type::HCI_COMMAND_PACKET:
-                format_to(ctx.out(), "HCI_COMMAND");
+                return format_to(ctx.out(), "HCI_COMMAND");
                 break;
             case h5_pkt_type::ACL_DATA_PACKET:
-                format_to(ctx.out(), "ACL_DATA");
+                return format_to(ctx.out(), "ACL_DATA");
                 break;
             case h5_pkt_type::SYNC_DATA_PACKET:
-                format_to(ctx.out(), "SYNC_DATA");
+                return format_to(ctx.out(), "SYNC_DATA");
                 break;
             case h5_pkt_type::HCI_EVENT_PACKET:
-                format_to(ctx.out(), "HCI_EVENT");
+                return format_to(ctx.out(), "HCI_EVENT");
                 break;
             case h5_pkt_type::RESET_PACKET:
-                format_to(ctx.out(), "RESET");
+                return format_to(ctx.out(), "RESET");
                 break;
             case h5_pkt_type::VENDOR_SPECIFIC_PACKET:
-                format_to(ctx.out(), "VENDOR_SPECIFIC");
+                return format_to(ctx.out(), "VENDOR_SPECIFIC");
                 break;
             case h5_pkt_type::LINK_CONTROL_PACKET:
-                format_to(ctx.out(), "LINK_CONTROL");
+                return format_to(ctx.out(), "LINK_CONTROL");
                 break;
             default:
-                format_to(ctx.out(), "UNKNOWN[{:#04x}]", static_cast<uint32_t>(pkt_type));
+                return format_to(ctx.out(), "UNKNOWN[{:#04x}]", static_cast<uint32_t>(pkt_type));
         }
     }
 };
@@ -123,16 +122,16 @@ template <> struct formatter<UartParity>
         switch (parity)
         {
             case UartParityNone:
-                format_to(ctx.out(), "none");
+                return format_to(ctx.out(), "none");
                 break;
             case UartParityOdd:
-                format_to(ctx.out(), "odd");
+                return format_to(ctx.out(), "odd");
                 break;
             case UartParityEven:
-                format_to(ctx.out(), "even");
+                return format_to(ctx.out(), "even");
                 break;
             default:
-                format_to(ctx.out(), "unknown");
+                return format_to(ctx.out(), "unknown");
         }
     }
 };
@@ -151,16 +150,16 @@ template <> struct formatter<UartFlowControl>
         switch (flowControl)
         {
             case UartFlowControlNone:
-                format_to(ctx.out(), "none");
+                return format_to(ctx.out(), "none");
                 break;
             case UartFlowControlSoftware:
-                format_to(ctx.out(), "software");
+                return format_to(ctx.out(), "software");
                 break;
             case UartFlowControlHardware:
-                format_to(ctx.out(), "hardware");
+                return format_to(ctx.out(), "hardware");
                 break;
             default:
-                format_to(ctx.out(), "unknown");
+                return format_to(ctx.out(), "unknown");
         }
     }
 };
@@ -179,16 +178,16 @@ template <> struct formatter<asio::serial_port::parity>
         switch (parity)
         {
             case asio::serial_port::parity::none:
-                format_to(ctx.out(), "none");
+                return format_to(ctx.out(), "none");
                 break;
             case asio::serial_port::parity::odd:
-                format_to(ctx.out(), "odd");
+                return format_to(ctx.out(), "odd");
                 break;
             case asio::serial_port::parity::even:
-                format_to(ctx.out(), "even");
+                return format_to(ctx.out(), "even");
                 break;
             default:
-                format_to(ctx.out(), "unknown");
+                return format_to(ctx.out(), "unknown");
         }
     }
 };
@@ -207,16 +206,16 @@ template <> struct formatter<asio::serial_port::flow_control>
         switch (flowControl)
         {
             case asio::serial_port::flow_control::none:
-                format_to(ctx.out(), "none");
+                return format_to(ctx.out(), "none");
                 break;
             case asio::serial_port::flow_control::software:
-                format_to(ctx.out(), "software");
+                return format_to(ctx.out(), "software");
                 break;
             case asio::serial_port::flow_control::hardware:
-                format_to(ctx.out(), "hardware");
+                return format_to(ctx.out(), "hardware");
                 break;
             default:
-                format_to(ctx.out(), "unknown");
+                return format_to(ctx.out(), "unknown");
         }
     }
 };
