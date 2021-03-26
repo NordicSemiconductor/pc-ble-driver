c02e6cc255882e331f786c48560b47a50fcf909a
Ken A. Redergård <64542+kenr@users.noreply.github.com>
2021-03-26 18:46:39 +0100

    Update clang-tidy target

--- cmake/clang-dev-tools.cmake
+++ cmake/clang-dev-tools.cmake
@@ -1,50 +1,19 @@
-if(WIN32)
-    if (NOT CMAKE_GENERATOR STREQUAL "Ninja")
-        set(NOT_SUPPORTED_MESSAGE "clang-tidy is only supported with Ninja generator on Microsoft Windows.")
-        message(STATUS "${NOT_SUPPORTED_MESSAGE}")
-
-        # For the target not to fail:
-        add_custom_target(tidy COMMAND echo "${NOT_SUPPORTED_MESSAGE}")
-        return()
-    else()
-        find_package (Python COMPONENTS Interpreter)
-
-        if(NOT Python_Interpreter_FOUND)
-            set(PYTHON_MISSING_MESSAGE "Python interpreter not found, it is required for run-clang-tidy.py")
-            message(STATUS "${PYTHON_MISSING_MESSAGE}")
-            add_custom_target(tidy COMMAND echo "${PYTHON_MISSING_MESSAGE}")
-            return()
-        endif()
-    endif()
-endif()
-
-find_program(CLANG_TIDY NAMES run-clang-tidy-7.py run-clang-tidy.py)
-
-if(NOT CLANG_TIDY)
-    message(STATUS "Did not find clang-tidy, target tidy is disabled.")
-    message(STATUS "If clang-tidy is installed, make sure run-clang-tidy.py and clang-tidy is in PATH")
-
-    # For the target not to fail:
-    add_custom_target(tidy COMMAND echo "Clang-tidy is not installed")
+if(IGNORE_CLANG_TIDY)
+    message(STATUS "IGNORE_CLANG_TIDY is ON. Ignoring clang-tidy.")
+    set(CLANG_TIDY_PATH "")
 else()
-    message(STATUS "Found clang-tidy, use \"cmake --build . --target tidy\" to run it.")
+    find_program(CLANG_TIDY_PATH NAMES clang-tidy-9 clang-tidy-10 clang-tidy HINTS "C:/Program Files/LLVM/bin")
 
-    # This will create build/compile_commands.json
-    set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+    if(CLANG_TIDY_PATH)
+        # Need to pass c++17 to clang-tidy as an extra argument from cmake to clang-tidy
+        set(CMAKE_CXX_CLANG_TIDY  "${CLANG_TIDY_PATH};--extra-arg-before=-std=c++17")
 
-    # Configuration of rules are picked up from .clang-tidy
-    message(STATUS "Picking up clang-tidy rules from ${CMAKE_SOURCE_DIR}/.clang-tidy")
-    set(CLANG_TIDY_ARGS "-header-filter=.*")
+        if(WIN32)
+            set(CMAKE_CXX_CLANG_TIDY  "${CMAKE_CXX_CLANG_TIDY};--extra-arg-before=/EHsc")
+        endif()
 
-    if (WIN32)
-        add_custom_target(tidy
-            COMMAND ${Python_EXECUTABLE} ${CLANG_TIDY} ${CLANG_TIDY_ARGS} .
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        )
+        message(STATUS "clang-tidy linter found in directory ${CLANG_TIDY_PATH}")
     else()
-        add_custom_target(tidy
-            COMMAND ${CLANG_TIDY} ${CLANG_TIDY_ARGS} .
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        )
+        message(STATUS "clang-tidy linter not found, linting will not be done.")
     endif()
 endif()
