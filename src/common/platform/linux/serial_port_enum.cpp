52197b459aa6ff0accb711cb426c9aa2b6957c05
Ladislav Láska <krakonos@krakonos.org>
2022-03-15 15:01:43 +0100

    Added NRF52 dongle to list of recognized devices on Linux. (#251)
    NRF52 dongle can run the Connectivity firmware and works fine with the
    library. Unfortunately, the library does not recognize the port.
    
    This is an issue in the pc-ble-driver-py, where even the examples do not
    work.
    
    Co-authored-by: Ken A. Redergård <64542+kenr@users.noreply.github.com>

--- src/common/platform/linux/serial_port_enum.cpp
+++ src/common/platform/linux/serial_port_enum.cpp
@@ -46,7 +46,9 @@
 #include "serial_port_enum.h"
 
 const char *SEGGER_VENDOR_ID = "1366";
-const char *NXP_VENDOR_ID    = "0d28";
+const char *NXP_VENDOR_ID = "0d28";
+const char *NORDIC_SEMICONDUCTOR_VENDOR_ID = "1915";
+const char *NRF52_CONNECTIVITY_DONGLE_PID = "c00a";
 
 std::string to_str(const char *s)
 {
@@ -84,20 +86,30 @@ std::list<SerialPortDesc> EnumSerialPorts()
             udev_device_get_parent_with_subsystem_devtype(udev_tty_dev, "usb", "usb_device");
 
         std::string idVendor = to_str(udev_device_get_sysattr_value(udev_usb_dev, "idVendor"));
-        std::string manufacturer =
-            to_str(udev_device_get_sysattr_value(udev_usb_dev, "manufacturer"));
+        std::string idProduct = to_str(udev_device_get_sysattr_value(udev_usb_dev, "idProduct"));
+        std::string manufacturer = to_str(udev_device_get_sysattr_value(udev_usb_dev,"manufacturer"));
 
-        if (((idVendor == SEGGER_VENDOR_ID) || (idVendor == NXP_VENDOR_ID)) &&
-            ((manufacturer == "SEGGER") || (strncasecmp(manufacturer.c_str(), "arm", 3) == 0) ||
-             (strncasecmp(manufacturer.c_str(), "mbed", 4) == 0)))
+        if(
+          (
+            ((idVendor == SEGGER_VENDOR_ID) || (idVendor == NXP_VENDOR_ID))
+            && ((manufacturer == "SEGGER")
+                || (strncasecmp(manufacturer.c_str(), "arm", 3) == 0)
+                || (strncasecmp(manufacturer.c_str(), "mbed", 4) == 0))
+          )
+          || ((idVendor == NORDIC_SEMICONDUCTOR_VENDOR_ID) && (idProduct == NRF52_CONNECTIVITY_DONGLE_PID))
+          )
         {
-            std::string serialNumber =
-                to_str(udev_device_get_sysattr_value(udev_usb_dev, "serial"));
-            std::string idProduct =
-                to_str(udev_device_get_sysattr_value(udev_usb_dev, "idProduct"));
+            std::string serialNumber = to_str(udev_device_get_sysattr_value(udev_usb_dev, "serial"));
 
-            devices.push_back(
-                SerialPortDesc{devname, manufacturer, serialNumber, "", path, idVendor, idProduct});
+            devices.push_back(SerialPortDesc {
+              devname,
+              manufacturer,
+              serialNumber,
+              "",
+              path,
+              idVendor,
+              idProduct
+            });
         }
 
         udev_device_unref(udev_tty_dev);
